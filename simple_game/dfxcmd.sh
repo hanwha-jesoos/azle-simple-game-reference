1708d6c429e34b41322248bc4f3b2d859b58c7c4
jesoos <jesoos@hanwha.com>
2023-10-21 18:03:59 +0900

    Update

--- simple_game/dfxcmd.sh
+++ simple_game/dfxcmd.sh
@@ -1,13 +1,12 @@
-#@echo on
-#dfx identity use default
-#cd ../token
-#dfx canister call token initialize '("CozToken","COZ",10000)'
-#dfx canister call token addAdmin '("be2us-64aaa-aaaaa-qaabq-cai")'
-#dfx canister call token approve '("be2us-64aaa-aaaaa-qaabq-cai",1000)'
-#dfx canister call token transfer '("fhtr7-pkuc6-nbeix-in4ti-c2nyn-7aa3v-zdija-vwg7z-6z354-destd-bqe", 1000)'
-#dfx identity use second-identity
-#dfx canister call token approve '("be2us-64aaa-aaaaa-qaabq-cai",1000)'
-#cd ../simple_game
+dfx identity use default
+cd ../token
+dfx canister call token initialize '("CozToken","COZ",10000)'
+dfx canister call token addAdmin '("be2us-64aaa-aaaaa-qaabq-cai")'
+dfx canister call token approve '("be2us-64aaa-aaaaa-qaabq-cai",1000)'
+dfx canister call token transfer '("fhtr7-pkuc6-nbeix-in4ti-c2nyn-7aa3v-zdija-vwg7z-6z354-destd-bqe", 1000)'
+dfx identity use second-identity
+dfx canister call token approve '("be2us-64aaa-aaaaa-qaabq-cai",1000)'
+cd ../simple_game
 dfx identity use default
 dfx canister call simple_game initialize '("bkyz2-fmaaa-aaaaa-qaaaq-cai")'
 dfx canister call simple_game createCharacter '()'
